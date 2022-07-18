a1b0fa4569bc25e4d4871531eb054b00e895afff
Dobby <dy227474@gmail.com>
2022-07-18 16:49:41 +0900

    라우터 설정 페이지 추가

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './Home'
+import About from './About'
+
+export default createRouter({
+    // Hash mode 사용 | History mode는 서버에 따로 세팅을 해야 함
+    // https://google.com/#/search
+    // 특정 페이지에서 새로고침을 했을 때 페이지를 찾을 수 없다는 메시지를 방지
+    history: createWebHashHistory(),
+    // pages
+    routes: [
+        {
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/about',
+            component: About
+        }
+    ]
+})
