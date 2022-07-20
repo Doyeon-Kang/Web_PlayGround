239947d3ee4a9814fb8fb59c4ba42e2993b82ff0
Dobby <dy227474@gmail.com>
2022-07-20 15:37:27 +0900

    components: MovieItem.vue MovieList.vue 추가

---
+++ src/components/MovieList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <div class="inner">
+            <MovieItem v-for="movie in movies" :key="movie.imdbID" />
+        </div>
+    </div>
+</template>
+
+<script>
+import MovieItem from './MovieItem.vue'
+
+export default {
+    components: {
+    MovieItem
+},
+    data() {
+        return {
+            movies: []
+        }
+    }
+}
+</script>
