544f7bfc3cd8a06a23ba5b7917ab97a7639c3e08
Dobby <dy227474@gmail.com>
2022-07-20 15:19:10 +0900

    axios 추가 / OMDB api 활용 메소드 추가

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="container">
-        <input v-model="title" class="form-control" type="text" placeholder="Search for Movies, Series & more"/>
+        <input v-model="title" class="form-control" type="text" placeholder="Search for Movies, Series & more" @keyup.enter="apply"/>
         <div class="selects">
             <select v-model="$data[filter.name]" v-for="filter in filters" :key="filter.name" class="form-select">
                 <option v-if="filter.name === 'year'" value="">All Years</option>
                 <option v-for="item in filter.items" :key="item">{{ item }}</option>
             </select>
         </div>
+        <button class="btn btn-primary" @click="apply">Apply</button>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
+
     export default {
         data() {
             return {
@@ -40,6 +43,13 @@
                     }
                 ]
             }
+        },
+        methods: {
+            async apply() {
+                const OMDB_API_KEY = '7035c60c'
+                const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${this.title}&type=${this.type}&y=${this.year}&page=1`)
+                console.log(res)
+            }
         }
     }
 </script>
@@ -63,6 +73,12 @@
                 margin-right: 0;
             }    
         }
+    }
+    .btn {
+        width: 120px;
+        height: 50px;
+        font-weight: 700;
+        flex-shrink: 0; // flex 내부 요소 사이즈 축소 비율 지정
     }
 }
 
