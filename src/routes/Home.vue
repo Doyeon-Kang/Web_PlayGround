dae8df0b1a324b6296dd4cebdea5ce4ee2b57b2e
Dobby <dy227474@gmail.com>
2022-07-20 15:37:27 +0900

    components: MovieItem.vue MovieList.vue 추가

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,16 +1,19 @@
 <template>
     <Headline />
     <Search />
+    <MovieList />
 </template>
 
 <script>
     import Headline from '../components/Headline.vue'
     import Search from '../components/Search.vue'
+    import MovieList from '../components/MovieList.vue'
 
     export default {
         components: {
             Headline,
-            Search
+            Search,
+            MovieList
         }
     }
 </script>
