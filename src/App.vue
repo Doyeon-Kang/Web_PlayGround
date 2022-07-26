20224cba38b7d434ecbd36aee912b04106600766
Doyeon-Kang <z3169@naver.com>
2022-07-26 13:51:48 +0900

    Footer 생성

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Header></Header>
   <router-view></router-view>
+  <Footer></Footer>
 </template>
 
 <script>
   import Header from './components/Header.vue'
+  import Footer from './components/Footer.vue'
 
   export default {
     components: {
-      Header
+      Header,
+      Footer
     }
   }
 </script>
